83ccb8fc8bc815ba8fda958aaf3a5f0c29d28fef
sergonzalezgomez <sergonzalezgomez@gmail.com>
2020-04-29 16:49:21 +0200

    12. Modificado el metodo processCommand añadidos los metodos showAll() y showCommands()

--- CommandWords.java
+++ CommandWords.java
@@ -13,8 +13,8 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "go", "quit", "help", "look", "eat"
-    };
+            "go", "quit", "help", "look", "eat"
+        };
 
     /**
      * Constructor - initialise the command words.
@@ -37,5 +37,16 @@ public class CommandWords
         }
         // if we get here, the string was not found in the commands
         return false;
+    }
+
+    /** 
+     * Imprime por pantalla todos los comandos válidos 
+     */
+    public void showAll() {
+        String comandos = "";
+        for (String comando : validCommands) {
+            comandos += comando + " ";
+        } 
+        System.out.print(comandos + "\n");
     }
 }
--- Game.java
+++ Game.java
@@ -147,11 +147,11 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("You are lost. You are alone. You wander.");
         System.out.println("around at the university.");
         System.out.println();
-        System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("Your command words are: ");
+        parser.showCommands();
     }
 
     /** 
@@ -203,7 +203,7 @@ public class Game
     private void look() {
         System.out.println(currentRoom.getLongDescription());
     }
-    
+
     private void eat() {
         System.out.println("You have eaten now and you are not hungry any more.");
     }
